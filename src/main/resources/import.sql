233e3231b009609b05dd6a5d79cc6fbb8c119835
Mafdmoore <mafdmoore@gmail.com>
2018-04-03 16:56:04 +0100

    Adds initialiser data to database on startup.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO ACCOUNT (id, firstname, secondname, accountnumber) values ('1', 'John', 'Doe', '1234')
+INSERT INTO ACCOUNT (id, firstname, secondname, accountnumber) values ('2', 'Jane', 'Doe', '1235')
+INSERT INTO ACCOUNT (id, firstname, secondname, accountnumber) values ('3', 'Jim', 'Taylor', '1236')
